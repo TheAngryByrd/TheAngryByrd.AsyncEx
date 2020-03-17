f85a57ab376b01faede5647efa1810152a1d215e
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2020-03-17 16:58:58 -0400

    Add overload for CancellableTask

--- tests/TheAngryByrd.AsyncEx.Tests/Tests.fs
+++ tests/TheAngryByrd.AsyncEx.Tests/Tests.fs
@@ -4,22 +4,79 @@ open System
 open Expecto
 open TheAngryByrd.AsyncEx
 open TheAngryByrd.AsyncEx.Say
+open System.Threading
+open System.Threading.Tasks
+// open FSharp.Control.Tasks.CopiedDoNotReference.V2.ContextInsensitive
+open TAB.FSharp.Control.Tasks.V2.ContextInsensitive
+
+type CancellableTask<'a> = CancellationToken -> Task<'a>
+
+
+type Async =
+    static member WithCancellation(computation : Async<'T>, cancellationToken : CancellationToken) : Async<'T> =
+        async {
+            let! ct2 = Async.CancellationToken
+            use cts = CancellationTokenSource.CreateLinkedTokenSource (cancellationToken, ct2)
+            let tcs = new TaskCompletionSource<'T>()
+            use _reg = cts.Token.Register (fun () -> tcs.TrySetCanceled() |> ignore)
+            let inner =
+                async {
+                    try
+                      let! a = computation
+                      tcs.TrySetResult a |> ignore
+                    with ex ->
+                      tcs.TrySetException ex |> ignore
+                }
+            Async.Start (inner, cts.Token)
+            return! Async.AwaitTask tcs.Task
+        }
+    static member WithCancellation2 (cancellationToken : CancellationToken) computation =
+      Async.WithCancellation(computation, cancellationToken)
+
+
+[<AutoOpen>]
+module Extensions =
+    type AsyncBuilder with
+        member inline __.Bind(t : Task<'a>, cont) = async.Bind(t |> Async.AwaitTask, cont)
+        member inline __.Bind(t : Task, cont) = async.Bind(t |> Async.AwaitTask, cont)
+        member inline __.ReturnFrom(t : Task<'a>) = async.ReturnFrom(t |> Async.AwaitTask)
+        member inline __.ReturnFrom(t : Task) = async.ReturnFrom(t |> Async.AwaitTask)
+        member inline __.Bind(t : CancellableTask<'a>, cont : 'a -> Async<'b>) =
+            let augmented = async {
+                let! ct = Async.CancellationToken
+                return! t ct
+            }
+            async.Bind(augmented, cont)
+        member inline __.ReturnFrom(t : CancellableTask<'a>) =
+            let augmented = async {
+                let! ct = Async.CancellationToken
+                return! t ct
+            }
+            async.ReturnFrom augmented
+
 
 [<Tests>]
 let tests =
-    testList "samples" [
-        testCase "Add two integers" <| fun _ ->
-            let subject = Say.add 1 2
-            Expect.equal subject 3 "Addition works"
-        testCase "Say nothing" <| fun _ ->
-            let subject = Say.nothing ()
-            Expect.equal subject () "Not an absolute unit"
-        testCase "Say hello all" <| fun _ ->
-            let person = {
-                Name = "Jean-Luc Picard"
-                FavoriteNumber = 4
-                FavoriteColor = Red
-                DateOfBirth = DateTimeOffset.Parse("July 13, 2305")
+    testList "AsyncTests" [
+        testCaseAsync "Bindable CancellableTask" <| async {
+            let mutable wasCalled = false
+            let sideEffect (ct : CancellationToken) = task {
+                do! Task.Delay(TimeSpan.FromSeconds(5.), ct)
+                wasCalled <- true
+                return ()
             }
-            let subject = Say.helloPerson person
-            Expect.equal subject "Hello Jean-Luc Picard. You were born on 2305/07/13 and your favorite number is 4. You like Red." "You didn't say hello" ]
+
+            let inner = async {
+                do! sideEffect
+                Expect.isFalse wasCalled "Side effect should not occur"
+            }
+
+            use cts = new CancellationTokenSource ()
+            cts.CancelAfter(TimeSpan.FromSeconds(0.1))
+            try
+                do! Async.WithCancellation2 cts.Token inner
+            with :? TaskCanceledException as e ->
+                // Cancellation is the point here
+                ()
+        }
+    ]
